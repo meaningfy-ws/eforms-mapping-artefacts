162e5d190787cdabdbeaf3687bac8d2ad64a7df3
Rashif Ray Rahman <schiv@archlinux.org>
2024-07-31 16:43:50 +0600

    Clarify analysis scripts dependencies, track field-node patch
    - Add a convenience patch to help apply changes between field and node scripts
    - Clarify Python dependencies w/ dedicated `requirements.txt` file
    - Clarify/improve installation requirements in README
    - Remove unneded commented code (StringIO is implicit during printing)

--- analysis_scripts/cm_versioned_dump.py
+++ analysis_scripts/cm_versioned_dump.py
@@ -6,10 +6,8 @@ import pandas as pd
 # https://tech.marksblogg.com/git-track-changes-in-media-office-documents.html
 
 sheet_name = "Rules"
-# output = StringIO()
 df = pd.read_excel(sys.argv[1], sheet_name=sheet_name)
 df = df[df[['Min SDK Version', 'Max SDK Version']].notnull().any(axis=1)][df.columns[0:3]]\
   .to_csv(header=False,
           index=False)
-# print(output.getvalue())
 print(df)

dbe5862ca22233e604245133c8a2d3da3b997ac2
Rashif Ray Rahman <schiv@archlinux.org>
2024-10-10 22:30:26 +0600

    languageMap RML generation indent size 4

--- src/scripts/gen_newLangMap.sh
+++ src/scripts/gen_newLangMap.sh
@@ -7,7 +7,7 @@
 queryfile=scripts/construct_newLangMap.rq
 
 owlcmd="java -jar ../owlcli/owl-cli.jar"
-owlcmd_params_basic="--keepUnusedPrefixes"
+owlcmd_params_basic="--keepUnusedPrefixes --indentSize 4"
 owlcmd_params_prefixes="-prdf -prdfs -prr=http://www.w3.org/ns/r2rml# -prml=http://semweb.mmlab.be/ns/rml# -ptedm=http://data.europa.eu/a4g/mapping/sf-rml/"
 owlcmd_params_predOrder="--predicateOrder rdf:type --predicateOrder tedm:minSDKVersion --predicateOrder tedm:maxSDKVersion --predicateOrder rdfs:label --predicateOrder rdfs:comment --predicateOrder rml:logicalSource --predicateOrder rr:subjectMap --predicateOrder rr:predicateObjectMap --predicateOrder rml:source --predicateOrder rml:iterator --predicateOrder rml:referenceFormulation --predicateOrder rr:template --predicateOrder rml:reference --predicateOrder rr:class --predicateOrder rr:predicate --predicateOrder rr:objectMap --predicateOrder rml:reference --predicateOrder rml:languageMap"
 

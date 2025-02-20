62755d0a0f8aec885720ffe6a65a570f04aee30a
Rashif Ray Rahman <schiv@archlinux.org>
2025-02-20 20:38:22 +0600

    Update transformation scripts, add support for E1 "pmc" notice
    Add support for E1 for both examples and single source, while enhancing
    the examples script to detect aliases for E5 (compl) and E1 (pmc).

--- src/scripts/tx_example-multiver.sh
+++ src/scripts/tx_example-multiver.sh
@@ -10,7 +10,7 @@ rmlmapper_cmd="java -jar $rmlmapper"
 show_help() {
     echo "Usage: $0 -t|--type <type>"
     echo "Options:"
-    echo "  -t, --type         Specify the type of transformation. Must be 'cn', 'can', 'pin' or 'e5'."
+    echo "  -t, --type         Specify the type of transformation. Must be 'cn', 'can', 'pin', 'e5' / 'compl' or 'e1' / 'pmc'."
     echo "  -h, --help         Display this help message."
 }
 
@@ -40,8 +40,8 @@ if [[ -z "$type" ]]; then
 else
     # Convert type to lowercase for case-insensitive comparison
     type=$(echo "$type" | tr '[:upper:]' '[:lower:]')
-    if [[ "$type" != "cn" && "$type" != "can" && "$type" != "pin" && "$type" != "e5" ]]; then
-        echo "Error: type must be either 'cn', 'can', 'pin' or 'e5'"
+    if [[ "$type" != "cn" && "$type" != "can" && "$type" != "pin" && "$type" != "e5" && "$type" != "compl" && "$type" != "e1"  && "$type" != "pmc" ]]; then
+        echo "Error: type must be either 'cn', 'can', 'pin', 'e5', 'compl', 'e1' or 'pmc'"
         exit 1
     fi
 fi
@@ -49,8 +49,12 @@ fi
 # Check if at least one data file exists for the specified type
 data_file_pattern="data/${type}*_24_maximal*.xml"
 
-if [[ "$type" = "e5" ]]; then
-    data_file_pattern="data/E5*_*.xml"    
+if [[ "$type" = "e5" || "$type" = "compl" ]]; then
+    data_file_pattern="data/E5*_*.xml"
+fi
+
+if [[ "$type" = "e1" || "$type" = "pmc" ]]; then
+    data_file_pattern="data/E1*_*.xml"
 fi
 
 if ! ls $data_file_pattern 1> /dev/null 2>&1; then
@@ -64,8 +68,12 @@ if [[ "$type" != "cn" ]]; then
     additional_mappings="mappings-${type}/*"
 fi
 
-if [[ "$type" == "e5" ]]; then
+if [[ "$type" = "e5" || "$type" = "compl" ]]; then
     additional_mappings="mappings-can/*"
+fi
+
+if [[ "$type" = "e1" || "$type" = "pmc" ]]; then
+    additional_mappings="mappings-pin/*"
 fi
 
 # Transform SDK example reference data for all versions
@@ -78,8 +86,12 @@ for i in $(ls -dv mappings-1*); do
         data_name="${type}-only_24_maximal"
     fi
 
-    if [[ "$type" = "e5" ]]; then
+if [[ "$type" = "e5" || "$type" = "compl" ]]; then
         data_name="E5_minimal"
+    fi
+
+if [[ "$type" = "e1" || "$type" = "pmc" ]]; then
+        data_name="E1_minimal"
     fi
 
     data_file="data/$data_name-$version.xml"
